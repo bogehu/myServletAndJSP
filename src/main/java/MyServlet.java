1fd0cd214904c6bfc435972161c2d030129605e4
bogehu <bogehu@hucongzheng.cn>
2016-08-23 11:07:32 +0800

    Initial commit

---
+++ src/main/java/MyServlet.java
@@ -0,0 +1,61 @@
+/**
+ * Created by bogehu on 16/8/22.
+ */
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class MyServlet extends HttpServlet {
+    public MyServlet() {
+        super();
+    }
+
+    public void destroy() {
+        super.destroy();
+    }
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+        out.println("<HTML>");
+        out.println("  <HEAD><TITLE>这是我的第一个Servlet</TITLE></HEAD>");
+        out.println("  <BODY>");
+        out.print("HelloWorld!  This is ");
+        out.print(this.getClass());
+        out.println(", using the GET method!");
+        out.println("</BODY>");
+        out.println("</HTML>");
+        out.flush();
+        out.close();
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+        out.println("<HTML>");
+        out.println("  <HEAD><TITLE>这是我的第一个Servlet</TITLE></HEAD>");
+        out.println("  <BODY>");
+        out.print(" HelloWorld!  This is ");
+        out.print(this.getClass());
+        out.println(", using the POST method");
+        out.println("  </BODY>");
+        out.println("</HTML>");
+        out.flush();
+        out.close();
+    }
+
+
+
+}
